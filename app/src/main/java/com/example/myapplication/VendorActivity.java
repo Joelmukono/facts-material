29a5838d9cd963c5dd5c2532b8e40c12b06e7338
MarkWatitwa <markwatitwa98@gmail.com>
2020-03-30 18:37:10 +0300

    renamed home activity

--- app/src/main/java/com/example/myapplication/VendorActivity.java
+++ app/src/main/java/com/example/myapplication/VendorActivity.java
@@ -29,7 +29,7 @@ public class VendorActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_home_vendor);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
